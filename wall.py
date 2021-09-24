32e079be2566b5866073aab356467ac1a07a3299
BigBassWheel <61646814+BigBassWheel@users.noreply.github.com>
2021-09-23 23:28:32 -0700

    Bozo was said hello to

--- wall.py
+++ wall.py
@@ -26,25 +26,22 @@ enhancer = ImageEnhance.Contrast(img)
 draw = ImageDraw.Draw(img)
 j = width
 cd = 0
-line_width = width // 2
+line_width = random.randrange(1, 100)
 
 while True:
     for i in range(width):
 
-        #color1 = random.randrange(1, 100)
-        color1 = 50
-        color2 = 0
-        #color2 = random.randrange(1, 20)
-        color3 = random.randrange(1, 100)
-        line_width -= 1
+        color1 = random.randrange(1, 200)
+        color2 = random.randrange(1, 200)
+        color3 = random.randrange(1, 200)
         cd += random.randint(1,1)
-        #draw.arc([(0, 0), (i, height)], 0, 360, fill=(color1, color2, color3), width=(axis_tilt))
-        #draw.arc([(width - i, 0), (width, height)], 0, 360, fill=(color1, color2, color3), width=(axis_tilt))
-        draw.line([(0, 0), (width, height)], fill=(color1 + cd, color2 + cd, 40), width=(line_width), joint="curve")
+        draw.arc([(0, 0), (i, height)], 0, 360, fill=(color1, color2, color3), width=(line_width))
+        draw.arc([(width - i, 0), (width, height)], 0, 360, fill=(color1, color2, color3), width=(line_width))
+        #draw.line([(0, 0), (width, height)], fill=(color1 + cd, color2 + cd, 40), width=(line_width), joint="curve")
         #draw.line([(0, i), (width, i * 4 + axis_tilt)], fill=(color2, color1, color3), width=(abs(axis_tilt // 2)))
         #img = enhancer.enhance(1.1)
 
-    img.show()
-    img.save("Image" + str(color1) + ".png")
+    #img.show()
+    img.save("Image.png")
 
-    time.sleep(30)
+    time.sleep(5)
