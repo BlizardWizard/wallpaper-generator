fa92e88ef5c81a01996e702110e05bea0f47bc90
BlizardWizard <ivar369@gmail.com>
2021-09-26 21:52:48 -0700

    update thing

--- Drawdata.py
+++ Drawdata.py
@@ -1,8 +1,14 @@
+from random import randrange
+from time import time
+
 class Drawdata:
     xy1 = []
     xy2 = []
     fill = []
     line_width = []
+    drawtype = 0
+    def __init__(self):
+        self.birth = time()
 
     permiateScale = 100
     def permiateX(self):
@@ -11,12 +17,13 @@ class Drawdata:
             self.xy1[i] = (self.xy1[i][0] - self.permiateScale, self.xy1[i][1])
             self.xy2[i] = (self.xy2[i][0] - self.permiateScale, self.xy2[i][1])
     
-    def reverseCycle(self, width):
-        # pre: must be at end of cycle, or beginning or cycle
-        # required pass width of screen
-        # post: reverses direction of permiation
-        # is void
-        if self.xy2[len(self.xy2) - 1][0] <= width + self.permiateScale: # at end of cycle
-            self.permiateScale *= -1
-        elif self.xy1[0][0] >= 0: # at begining of new cycle
-            self.permiateScale *= -1
+    def age(self):
+        return time() - self.birth
+
+    def randomizeY(self):
+        randScale = 2
+        for i in range(len(self.xy1)):
+            rand1 = randrange(-randScale, randScale)
+            self.xy1[i] = (self.xy1[i][0], self.xy1[i][1] + rand1)
+            rand2 = randrange(-randScale, randScale)
+            self.xy2[i] = (self.xy2[i][0], self.xy2[i][1] + rand2)
--- wall.py
+++ wall.py
@@ -31,26 +31,49 @@ draw = ImageDraw.Draw(img)
 j = width
 cd = 0
 
-data = Drawdata()
-for i in range(width):
-    cd = random.randint(-100,100)
-    data.xy1.append((0, i))
-    data.xy2.append((width*10, i + cd))
-    data.fill.append((random.randrange(1, 40), random.randrange(1, 2), random.randrange(1, 60)))
-    data.line_width.append(random.randrange(1, 20))
+def generateData():
+    data = Drawdata()
+    rand = random.randint(1, 2)
+    if rand == 1:
+        data.drawtype = 1
+        for i in range(width):
+            cd = random.randint(-100,100)
+            data.xy1.append((0, i))
+            data.xy2.append((width*10, i + cd))
+            data.fill.append((random.randrange(1, 40), random.randrange(1, 2), random.randrange(1, 60)))
+            data.line_width.append(random.randrange(1, 20))
+    elif rand == 2:
+        data.drawtype = 2
+        for i in range(width):
+            data.xy1.append((0, 0))
+            data.xy2.append((i, height))
+            data.fill.append((random.randrange(1, 40), random.randrange(1, 2), random.randrange(1, 60)))
+            data.line_width.append(random.randrange(1, 20))
+    return data
+data = generateData()
 
 while True:
     for i in range(width):
         
-        #draw.arc([(0, 0), (i, height)], 0, 360, fill=(color1, color2, color3), width=(line_width))
-        #draw.arc([(width - i, 0), (width, height)], 0, 360, fill=(color1, color2, color3), width=(line_width))
+
         #draw.line([(0, 0), (width, height)], fill=(color1 + cd, color2 + cd, 40), width=(line_width), joint="curve")
         #img = enhancer.enhance(1.1)
 
-        draw.line([data.xy1[i], data.xy2[i]], fill=data.fill[i], width=data.line_width[i])
+        if data.drawtype == 1:
+            draw.line([data.xy1[i], data.xy2[i]], fill=data.fill[i], width=data.line_width[i])
+        elif data.drawtype == 2:
+            draw.arc([data.xy1[i], data.xy2[i]], 0, 360, fill=data.fill[i], width=data.line_width[i])
+            # draw.arc([(width - i, 0), (width, height)], 0, 360, fill=(color1, color2, color3), width=(line_width))
+
+        print(data.age())
+        if data.age() > 3:
+            del data
+            data = generateData()
+            print("updated")
 
     data.permiateX()
-    data.reverseCycle(width)
+    # data.randomizeY()
+    # data.reverseCycle(width)
     img.save("image.png")
     ctypes.windll.user32.SystemParametersInfoW(20, 0, os.getcwd()+"\image.png" , 0)
 
